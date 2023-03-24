cfef765e384928035f35486fb6c9364bdec3e026
adiljamal754 <https://github.com/adiljamal754>
2023-03-24 15:28:56 +0200

    Updated the UI and configured viewBinding

--- app/src/main/java/com/example/currencyconversorapt/MainActivity.java
+++ app/src/main/java/com/example/currencyconversorapt/MainActivity.java
@@ -1,33 +1,20 @@
 package com.example.currencyconversorapt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import com.example.currencyconversorapt.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
-    public void Convert(View view){
 
-        //pegar o componente da tela
-        EditText valueText = findViewById(R.id.valueET);
-        TextView result = findViewById(R.id.currencyTV);
-
-        // pegar e converter o valor do component da tela
-        double value = Double.parseDouble(valueText.getText().toString());
-
-        final double dollar = 63.10;
-
-        result.setText("" + Math.round(value / dollar));
-
-    }
 
 }
